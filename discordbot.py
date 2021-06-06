d60ff8e4b56aa08452e469dbce5e3d19bc863a9e
kazn777 <71267891+kazn777@users.noreply.github.com>
2021-06-06 16:34:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,7 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+client = discord.Client()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -17,5 +18,14 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/csm':
+        await message.channel.send('Choco stupid mountain')
+    
+    
 bot.run(token)
