e19dabc4e65eaaa6c7d9bd99c65a4b0db937bf70
kazn777 <71267891+kazn777@users.noreply.github.com>
2021-06-13 22:39:38 +0900

    Embed

--- .history/discordbot_20210613223902.py
+++ .history/discordbot_20210613223902.py
@@ -24,7 +24,7 @@ async def csm(ctx):
 
 @bot.command()
 async def csmcsm(ctx):
-    embed = discord.Embed(title="choco stupid mountain",description="choco stupid mountain",color=0xff0000)
+    embed = discord.Embed(title="choco stupid mountain",description="choco stupid mountain",,color=0xff0000)
     await ctx.send(embed=embed)
 
 bot.run(token)
