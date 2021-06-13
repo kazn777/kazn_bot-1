e05c65664b22608ad9928602982e83fa86ad2b99
kazn777 <71267891+kazn777@users.noreply.github.com>
2021-06-13 22:20:54 +0900

    Embed

--- .history/discordbot_20210613221918.py
+++ .history/discordbot_20210613221918.py
@@ -1,4 +1,6 @@
+
 from discord.ext import commands
+from discord.ext.commands import Bot
 import os
 import traceback
 
--- .history/discordbot_20210613221927.py
+++ .history/discordbot_20210613221927.py
@@ -1,4 +1,6 @@
+import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
 import os
 import traceback
 
