0f40a03126736839296595b9c907c57884091aed
kazn777 <71267891+kazn777@users.noreply.github.com>
2021-07-15 23:27:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def database(ctx):
-    embed = discord.Embed(title="kazn's g spreadsheets",description="",color=0xff0000)
+    embed = discord.Embed(title="kazn's google spreadsheets",description="",color=0xff0000)
     embed.add_field(name="MK64 Past events list",value="[link](https://docs.google.com/spreadsheets/d/1OLVssG1cnNiaQas5shl44awqAdrW6ahQbfqpndac3BY/edit#gid=552396019)")
     embed.add_field(name="SGDQ2021マリオカート64解説用資料",value="[link](https://docs.google.com/spreadsheets/d/1GCy4gfZnVtDeUlMVFw1lt5kJ3rTEhjW3L4Ec0H9vPnI/edit#gid=1085278230)")
     embed.add_field(name="VAトーナメント参加マニュアル",value="[link](https://docs.google.com/spreadsheets/d/1npNm3upMzT4lSYMAUPWwOY4NbTKie9p2Ccfc33ViPqo/edit#gid=0)")
