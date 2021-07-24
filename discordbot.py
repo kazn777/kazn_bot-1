533f0c002301abbed0b687e6eeed282af9e0376f
kazn777 <71267891+kazn777@users.noreply.github.com>
2021-07-24 16:49:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,5 +23,21 @@ async def database(ctx):
     embed.add_field(name="VAトーナメント参加マニュアル",value="[link](https://docs.google.com/spreadsheets/d/1npNm3upMzT4lSYMAUPWwOY4NbTKie9p2Ccfc33ViPqo/edit#gid=0)")
 
     await ctx.send(embed=embed)
+    
+@bot.command()
+async def cmsg(ctx):
+    embed = discord.Embed(title="choco stupid mountain",description="",url = "https://clips.twitch.tv/GoodReliableArmadilloDoggo-QAW30SL4Rrgfkdrl",color=0xff0000)
+    embed.add_field(name="longver",value="[link](https://clips.twitch.tv/DeadNaiveTurnipLeeroyJenkins-oe9egb9VAYg4jzGg)")
+    embed.add_field(name="Choco nice Mountain",value="[link](https://clips.twitch.tv/BetterFastKuduM4xHeh-HhQM5C9nQVjs1QYe)")
+    embed.add_field(name="CNM-> CSM",value="[link](https://clips.twitch.tv/BitterObeseFungusPanicBasket-yacmSW8J-Wsb20tr)")
+    embed.add_field(name="mkdsabel ver",value="[link](https://clips.twitch.tv/ToughBenevolentRatBuddhaBar-AiWkoMrPsoSImLYy)")
+    embed.add_field(name="Ce_Mec ver",value="[link](https://clips.twitch.tv/GentleAltruisticCamelKappaWealth-zYGgXXazVl4hZyee)")
+    embed.add_field(name="TMP ver",value="[link](https://clips.twitch.tv/FunGracefulSparrowRaccAttack-qeFSem4jpPlkoXzi)")
+    embed.add_field(name="king_kelp CSM comment",value="[link](https://clips.twitch.tv/FitSpoopyTaroNerfBlueBlaster-Wkn_pDhjjTQ1P98p)")
+    embed.add_field(name="MySong abney reaction",value="[link](https://clips.twitch.tv/StylishWittyPigANELE-XqeuXk2RjskQSS1f)")
+    embed.add_field(name="Mysong listen MR",value="[link](https://clips.twitch.tv/HardEnticingGrouseCoolStoryBob-D-GF4BDJaNoBV-mT)")
+    await ctx.send(embed=embed)
+   
+
 
 bot.run(token)
